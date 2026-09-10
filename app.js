521d7e18f6f32507e4e3e97fe03bd4a203954fc7
Pasang Tamang <pasanghilp@gmail.com>
2026-09-10 12:00:41 +0545

    working with path module

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function sayHello(name){
     console.log(`hi ${name}`);
 }
--- import.js
+++ import.js
@@ -3,7 +3,8 @@ const logger = require('./logger');
 
 logger.log('hello');
 */
-
+console.log(__filename);
+console.log(__dirname);
 const log = require('./logger');
 
 log('say hello to my little friend');
