d91db9c1add0f8b1da51594daa6c2e5c3c1cafca
Pasang Tamang <pasanghilp@gmail.com>
2026-09-10 11:37:18 +0545

    Importing and exporting modules

---
+++ import.js
@@ -0,0 +1,9 @@
+/*
+const logger = require('./logger');
+
+logger.log('hello');
+*/
+
+const log = require('./logger');
+
+log('say hello to my little friend');
---
+++ logger.js
@@ -0,0 +1,11 @@
+var url = "http://pasangloger.com/log";
+
+function log(message) {
+    console.log(message);
+}
+
+/*
+module.exports.log = log;
+*/
+
+module.exports = log;
