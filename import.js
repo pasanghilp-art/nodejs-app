73be41245b610ee372652cda5512e037a21c6c8d
Pasang Tamang <pasanghilp@gmail.com>
2026-09-11 10:30:22 +0545

    using EventEmitter as extend for custom class

--- import.js
+++ import.js
@@ -3,8 +3,20 @@ const logger = require('./logger');
 
 logger.log('hello');
 */
+
+/*
 console.log(__filename);
 console.log(__dirname);
 const log = require('./logger');
 
 log('say hello to my little friend');
+*/
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+logger.on('messageLogged', (arg)=>{
+    console.log('messageLogged',arg)
+});
+
+logger.log('message');
--- logger.js
+++ logger.js
@@ -1,11 +1,24 @@
 var url = "http://pasangloger.com/log";
-
+/*
 function log(message) {
     console.log(message);
 }
 
-/*
 module.exports.log = log;
 */
 
+/*
 module.exports = log;
+*/
+
+const EventEmitter = require('events');
+
+class Logger extends EventEmitter{
+    log(message){
+        console.log(message);
+
+     this.emit('messageLogged', {id:1, url: 'https://'});
+    }
+}
+
+module.exports= Logger;
